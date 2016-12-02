82b959b1bcc1757874f51be7ead00c26ad4a87ff
SARISCJ <saracastillo0@gmail.com>
2016-12-02 13:22:02 -0500

    scroll

--- js/index.js
+++ js/index.js
@@ -18,6 +18,20 @@ $('.navbar-collapse ul li a').click(function() {
     $('.navbar-toggle:visible').click();
 });
 
+/*###### SCROLL ######*/
+$('a[href*="#"]:not([href="#"])').click(function() {
+ if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+   var target = $(this.hash);
+   target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+   if (target.length) {
+     $('html, body').animate({
+       scrollTop: target.offset().top
+     }, 1000);
+     return false;
+   }
+ }
+});
+
 /*###### SKILLS SECTION ######*/
 $( document ).ready(function() {
   var $developmentWrapper = $(".development-wrapper");
